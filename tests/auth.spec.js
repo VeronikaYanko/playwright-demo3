2fa7ccb6afd8ba73470cc961fcc402b64dd86451
VeronikaYanko <103013096+VeronikaYanko@users.noreply.github.com>
2023-09-23 15:09:24 -0700

    update

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,14 +1,25 @@
 const { test, expect } = require('@playwright/test');
 
-test.describe('Aunthentication & Authorization', async () => {
+test.describe('Authentication & Authorization', () => {
     test('Sign in with existing credentials', async ({ page }) => {
         await page.goto('https://coding.pasv.us/user/login')
 
-        await page.locator('#normal_login_email').fill('veroyanko@gmail.com')
-        await page.locator('#normal_login_password').fill('Niiko4ka')
+        await page.locator('#normal_login_email').fill('test@example.com')
+        await page.locator('#normal_login_password').fill('Qwerty!23')
         await page.locator('button[type="submit"]').click()
 
         await expect(page.locator('.ant-avatar-square')).toBeVisible()
+    })
 
+    test('Sign in with not existing credentials', async ({ page }) => {
+        await page.goto('https://coding.pasv.us/user/login')
+
+        await page.locator('#normal_login_email').fill('invalid@example.com')
+        await page.locator('#normal_login_password').fill('invalid')
+        await page.locator('button[type="submit"]').click()
+
+        const toast = page.locator('.ant-notification-notice-message')
+        await expect(toast).toBeVisible()
+        await expect(toast).toHaveText('User login. Fail')
     })
 })
