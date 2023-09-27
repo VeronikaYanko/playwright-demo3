0a04c738ece87ae0397f8c8df1a7b1e97281fdfe
VeronikaYanko <103013096+VeronikaYanko@users.noreply.github.com>
2023-09-26 21:29:56 -0700

    update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,34 +1,34 @@
-// @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const {defineConfig, devices} = require('@playwright/test')
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
- */ require('dotenv').config();
+ */
+require('dotenv').config()
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
   testDir: './tests',
-  workers: 1 ,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-   baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL,
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retrain-on-failure',
+    video: 'retrain-on-failure',
+    testIdAttribute: 'data-qa',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: {...devices['Desktop Chrome']},
     },
-      ],
-
-    });
-
+  ],
+})
